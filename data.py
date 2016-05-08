249cab9bdbdbd2e290f18336fd9f18f50533ba8f
Akashdeep Goel <akashdeep.goel1996@gmail.com>
2016-05-08 17:41:55 +0530

    adds raw data

---
+++ data.py
@@ -0,0 +1,11 @@
+#File containing dictionary to be used for similarity comparison
+
+#Scale : 1 to 5
+
+people = {	'Akashdeep Goel': {'Call of Duty: Modern Warfare': 3.5, 'Grand Theft Auto :V': 4.5, 'Just Cause 3': 4.0, 'Far Cry : Primal': 4.0, 'Assassin Creed Syndicate': 3.0, 'Crysis 3': 3.0},
+			'Gene Seymour': {'Call of Duty: Modern Warfare': 3.0, 'Grand Theft Auto :V': 3.5, 'Just Cause 3': 1.5, 'Far Cry : Primal': 5.0, 'Crysis 3': 3.0, 'Assassin Creed Syndicate': 3.5},
+			'Michael Phillips': {'Call of Duty: Modern Warfare': 2.5, 'Grand Theft Auto :V': 3.0, 'Far Cry : Primal': 3.5, 'Crysis 3': 4.0},
+			'Claudia Puig': {'Grand Theft Auto :V': 3.5, 'Just Cause 3': 3.0,	'Crysis 3': 4.5, 'Far Cry : Primal': 4.0,	'Assassin Creed Syndicate': 2.5},
+			'Mick LaSalle': {'Call of Duty: Modern Warfare': 3.0, 'Grand Theft Auto :V': 4.0, 'Just Cause 3': 2.0, 'Far Cry : Primal': 3.0, 'Crysis 3': 3.0, 'Assassin Creed Syndicate': 2.0},
+			'Jack Matthews': {'Call of Duty: Modern Warfare': 3.0, 'Grand Theft Auto :V': 4.0, 'Crysis 3': 3.0, 'Far Cry : Primal': 5.0, 'Assassin Creed Syndicate': 3.5},
+			'Toby': {'Grand Theft Auto :V':4.5,'Assassin Creed Syndicate':1.0,'Far Cry : Primal':4.0}}
